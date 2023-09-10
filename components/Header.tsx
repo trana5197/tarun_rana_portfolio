86999a1a010c2ab14d4aa0cc69417aafff330878
Tarun Rana <92117730+trana5197@users.noreply.github.com>
2023-09-10 18:03:39 -0500

    Updated active link in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ function Header() {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "text-gray-950 dark:text-gray-300":
+                    "text-gray-950 dark:text-white":
                       activeSection === link.name,
                   },
                 )}
